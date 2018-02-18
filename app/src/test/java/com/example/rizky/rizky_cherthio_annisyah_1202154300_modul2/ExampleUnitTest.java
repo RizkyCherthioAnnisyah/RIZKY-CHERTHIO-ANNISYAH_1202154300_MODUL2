a401c130a6195352c78cb11faf34ca5549096ecf
RizkyCherthioAnnisyah <36083140+RizkyCherthioAnnisyah@users.noreply.github.com>
2018-02-18 16:44:33 +0700

    push pertama kali

---
+++ app/src/test/java/com/example/rizky/rizky_cherthio_annisyah_1202154300_modul2/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.example.rizky.rizky_cherthio_annisyah_1202154300_modul2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
